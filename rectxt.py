ecbf4c446989aa44ad745123720ed8bee938b8c9
Hari0399 <43031560+Hari0399@users.noreply.github.com>
2018-11-15 16:19:15 +0530

    Add files via upload

---
+++ rectxt.py
@@ -0,0 +1,11 @@
+dl303@rathore:~/Hari$ python3 rec.py
+ALSA lib pcm_dsnoop.c:618:(snd_pcm_dsnoop_open) unable to open slave
+ALSA lib pcm.c:2495:(snd_pcm_open_noupdate) Unknown PCM cards.pcm.rear
+ALSA lib pcm.c:2495:(snd_pcm_open_noupdate) Unknown PCM cards.pcm.center_lfe
+ALSA lib pcm.c:2495:(snd_pcm_open_noupdate) Unknown PCM cards.pcm.side
+ALSA lib pcm_route.c:867:(find_matching_chmap) Found no matching channel map
+ALSA lib pcm_route.c:867:(find_matching_chmap) Found no matching channel map
+ALSA lib pcm_route.c:867:(find_matching_chmap) Found no matching channel map
+Say Something!
+You said: hello hello hello hello
+
